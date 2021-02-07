90fee8b96975d8d2e3f534fe426fa835a1ecd5ca
Josh Stout <joshstout@gmail.com>
2021-02-07 00:25:44 -0800

    initial with xo

---
+++ .xo-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  space: 2,
+  semicolon: false,
+  prettier: false,
+  settings: {
+    parserOptions: {
+      project: './tsconfig.xo.json'
+    }
+  },
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const a = 5
+
+function asdf() {
+  return 18
+}
