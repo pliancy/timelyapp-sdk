52e680ce7bc51b4ab493a8d18af0f407299953f8
Justin <justin.mcmahon@pliancy.com>
2022-09-26 12:16:26 -0700

    fix(projects): HALP-7462 getByExternalId should return array
    BREAKING CHANGE: projects.getByExternalId was returning a single project matching the given id,
    which would be the first returned. This updates the method to return all matching projects.

--- src/lib/projects/projects.test.ts
+++ src/lib/projects/projects.test.ts
@@ -1,7 +1,29 @@
 import { Projects } from './projects'
+import { axiosMock } from '../../../__fixtures__/axios'
+import { TimelyProject } from '../types'
 
 describe('Projects', () => {
-    it('creates', () => {
-        expect(new Projects({} as never, {} as never)).toBeTruthy()
+    let projects: Projects
+
+    beforeEach(
+        () => (projects = new Projects(axiosMock, { token: 'token', accountId: 'accountId' })),
+    )
+
+    describe('getByExternalId', () => {
+        it(`returns an empty array given getAll doesn't behave as expected`, async () => {
+            jest.spyOn(projects, 'getAll').mockResolvedValue({} as never)
+            await expect(projects.getByExternalId('CUST')).resolves.toEqual([])
+        })
+
+        it(`returns an empty array given no matches`, async () => {
+            jest.spyOn(projects, 'getAll').mockResolvedValue([{ external_id: 'NUNYA' } as never])
+            await expect(projects.getByExternalId('CUST')).resolves.toEqual([])
+        })
+
+        it(`returns matching projects`, async () => {
+            const p = [{ external_id: 'CUST' }] as never as TimelyProject[]
+            jest.spyOn(projects, 'getAll').mockResolvedValue(p)
+            await expect(projects.getByExternalId('CUST')).resolves.toEqual(p)
+        })
     })
 })
